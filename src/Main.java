844e56039bd8b7adf593bdbb95653bfdc73b4f10
SpaciousCoder78 <88923986+SpaciousCoder78@users.noreply.github.com>
2022-12-25 16:43:32 +0530

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("----------------------------------Coin Toss--------------------------------");
+        System.out.println("Heads or Tails? Press 0 for Heads, 1 for tails");
+        Scanner choice= new Scanner(System.in);
+        int choices = choice.nextInt();
+        if (choices==0){
+            System.out.println("You Chose: Heads");
+        } else if (choices==1) {
+            System.out.println("You Chose: Tails");
+        }
+        Random random = new Random();
+        int randomint = random.nextInt(1);
+        if (randomint==0){
+            System.out.println("RESULT: Heads");
+        }
+        if (randomint==1){
+            System.out.println("RESULT: Tails");
+        }
+        System.out.println("Do you want to flip again? Press 1 to continue");
+        int option = choice.nextInt();
+        if (option==1){
+            main(null);
+        }
+
+    }
+}
